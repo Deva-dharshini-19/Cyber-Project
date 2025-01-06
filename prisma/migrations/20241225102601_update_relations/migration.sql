faea85bbb3c9998d5585ebe42b01d7a05c75a93c
Deva-dharshini-19 <devapoongodi2004@gmail.com>
2025-01-06 12:01:41 +0530

    Delete prisma directory

--- prisma/migrations/20241225102601_update_relations/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Attack" (
-    "id" SERIAL NOT NULL,
-    "attackType" VARCHAR(50) NOT NULL,
-    "ipAddress" VARCHAR(15),
-    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Attack_pkey" PRIMARY KEY ("id")
-);
